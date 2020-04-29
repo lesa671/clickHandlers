5f96650d0d6da619136ed909dc72ebdaac591439
Lesa Ducksworth <lducks67@gmail.com>
2020-04-28 21:30:35 -0500

    made page interactive

--- assets/javascript/clickhandler.js
+++ assets/javascript/clickhandler.js
@@ -18,24 +18,34 @@ let count = 0
 const countspan = document.querySelector('#countspan')
 const counter_button = document.querySelector('#counter_button')
 counter_button.addEventListener('click', function () {
- /* TODO 2
-    *
-    * Fill in this function so that it increments
-    * (adds one to) the variable named count,
-    * then updates the inner text of "countspan"
-    * to show the current value of "count".
-    */
+    console.log('counter_button clicked')
+    // let newCount = parseInt(countspan.innerHTML) + 1
+    // countspan.innerHTML = newCount
+    count = count + 1
+    countspan.innerText = count
+
+    /* TODO 2
+       *
+       * Fill in this function so that it increments
+       * (adds one to) the variable named count,
+       * then updates the inner text of "countspan"
+       * to show the current value of "count".
+       */
 })
 
 const even_or_odd_button = document.querySelector('#even_or_odd_button')
 even_or_odd_button.addEventListener('click', function () {
+    console.log('even_or_odd_button clicked')
 
+    if (count % 2 === 0) {
+        alert('Count is even')
+    } else {
+        alert('Count is odd')
+    }
+})
 /* TODO 3
     *
     * Fill in this function so that it shows an
     * alert dialog stating whether the count variable
     * (from part 2, above) is even or odd.
     */
-
-
-})
